fe2e815869f4ddda164f31527ffa5d88de79d845
masatotakagi <mtakagi@gmail.com>
2022-02-20 14:01:58 -0800

    initial check in

---
+++ index.php
@@ -0,0 +1,126 @@
+<?php include "header.html"; ?>
+
+<center>
+<table>
+
+<tr><td>
+<h2 style="font-size: clamp(25px, 3vw, 30px);">Big Math Flash Cards</h2>
+</td></tr>
+
+<tr><td>
+<p>Flip the card just like a real flash card! Simple, clean and easy to use interface. The front side of the card shows the question and the back side of the card shows the possible answers. Just tap to flip the card. Once you answer the question correctly, flick the card off the screen by swiping up.</p?
+</td></tr>
+
+<tr><td>
+<center>
+<img border="none" vspace="5" width=200 src="MathCards1.png" style="max-width:24%;height:auto;">
+<img border="none" vspace="5" width=200 src="MathCards2.png" style="max-width:24%;height:auto;">
+<img border="none" vspace="5" width=200 src="MathCards3.png" style="max-width:24%;height:auto;">
+<img border="none" vspace="5" width=200 src="MathCards4.png" style="max-width:24%;height:auto;">
+</center>
+</td></tr>
+
+<tr><td>
+<h3>Features</h3>
+<ul>
+<li>Addition, Subtraction, Multiplication, Division
+<li>Randomly generated numbers
+<li>Score percentage and elapsed time displayed
+<li>Configurable number of questions and difficulty levels
+<li>Supports landscape or portrait card display modes
+<li>Turn all sounds on or off
+<li>Selectable flash card color themes
+<li>Other great features available via "In App Purchase"
+</ul>
+<br>
+<h3>Paid Features</h3>
+<ul>
+<li>Review Incorrect - review the questions that were answered incorrectly
+<li>Score Cards - view and review past scores and questions
+<li>Specific Numbers - practice a single or multiple specific numbers
+<li>Advanced Levels - advanced difficulty levels 4, 5 and 6
+<li>Mixed Operations - ability to have cards with mixed operators
+<li>Flash Card Mode - displays the answer instead of multiple choice
+<li>Custom Cards - ability to create your own custom questions
+</ul>
+</td></tr>
+
+<tr><td>
+<br>
+<center>
+<h3>Highly rated by teachers, parents and kids of all ages!</h3>
+<i>
+"I teach 1st grade and I use this app in my classroom.  I love it the kids are so engaged."<br><br>
+"Perfect app for my kids and my students. The kids love and want to play again and again!"<br><br>
+"I downloaded a lot of other apps but this app really helps my son improve his math skills."<br><br>
+"I love the simple and clean interface.  I highly recommend this app!"<br><br>
+</i>
+</center>
+<br>
+</td></tr>
+
+<tr><td>
+<h3>FAQs - Frequently Asked Questions</h3>
+
+<button type="button" class="collapsible"><li>How do I get to the next question/card?</button>
+<div class="content">
+  <p>Once you have answered the question correctly, please swipe up to get to the next card.  In flash card mode, you can swipe up to mark the card correct, and swipe down to mark the card incorrect.</p>
+</div>
+
+<button type="button" class="collapsible"><li>Is it possible to just show the answer, not multiple choice?</button>
+<div class="content">
+  <p>Yes, there is an "In App Purchase" called Flash Card Mode which enables you to just display the answer.  You can toggle this feature on/off in the settings screen.
+If you purchased the Full Paid School Version, this feature is included.</p>
+</div>
+
+<button type="button" class="collapsible"><li>How do I get to the mixed operations screen?</button>
+<div class="content">
+  <p>Please tap the arrow icon located at the top of the main screen.  Alternatively you can swipe left/right to flip the card.  
+The Full Paid School Version or the "In App Purchase" is required to use this feature.</p>
+</div>
+
+<button type="button" class="collapsible"><li>Is it possible to hide/show the card count display?</button>
+<div class="content">
+  <p>Yes, please tap the top right corner of the flash card to toggle between hiding and displaying the card count.</p>
+</div>
+
+<button type="button" class="collapsible"><li>What's the difference between the Free Version and Full Paid School Version?</button>
+<div class="content">
+  <p>The Full Paid School Version has all the available features unlocked.  The Free Version has "In App Purchases" where you can buy one feature at a time or all.  If you purchase 
+all the "In App Purchases", it will be exactly the same as the Full Paid School Version.  Many school districts do not allow apps with "In App Purchases".  If you plan on purchasing all the "In App Purchase" features, we recommend you buy
+the "Full Paid School Version" as it is easier to Family Share. </p>
+</div>
+
+<button type="button" class="collapsible"><li>How can I create my own specific questions?</button>
+<div class="content">
+  <p> To access the "Custom Cards" feature, just swipe left/right on the main screen and then select "Custom Cards". Enter your desired question and tap the "Add Question" button.  Once you are done adding questions, swipe left/right to see all the questions you have added.  Tap the "Start" button to begin.  An "In App Purchase" or the "Full Paid School Version" is required to used this feature.</p>
+</div>
+
+</td></tr>
+
+<tr><td>
+<br><br>
+<center>
+<h4>Available for the Apple iPhone, iPad and iPod Touch</h4>
+<br>
+</center>
+</td></tr>
+
+<tr><td>
+<center>
+<a target="new" border="none" href="https://itunes.apple.com/us/app/big-math-flash-cards-school/id1156731220?ls=1&mt=8">
+<img border="none" hspace="10" width="60" height="60" align="middle" src="MathCardsAppIconSchool.png"></a>
+<h4 style="font-size: clamp(14px, 2vw, 20px);">Full Paid School Version</h4><br>
+</center>
+<br>
+</td></tr>
+
+<tr><td>
+<center>
+<a target="new" border="none" href="https://itunes.apple.com/us/app/math-cards-flash-flashcards/id929492658?ls=1&mt=8">
+<img border="none" hspace="10" width="60" height="60" align="middle" src="MathCardsAppIconFree.png"></a>
+<h4 style="font-size: clamp(14px, 2vw, 20px);">Free Version with In App Purchase</h4><br>
+</center>
+</td></tr>
+
+<?php include "footer.html"; ?>
---
+++ math-flash-cards-app.php
@@ -0,0 +1,67 @@
+<?php include "header.html"; ?>
+
+<tr><td valign="top" width="430">
+<a href="MathCards1.png" rel="lightbox[MathCards]"><img border="none" vspace="5" width=200 src="MathCards1.png"></a>
+<a href="MathCards2.png" rel="lightbox[MathCards]"><img border="none" vspace="5" width=200 src="MathCards2.png"></a>
+<a href="MathCards3.png" rel="lightbox[MathCards]"></a>
+<a href="MathCards4.png" rel="lightbox[MathCards]"></a>
+<a href="MathCards5.png" rel="lightbox[MathCards]"></a>
+<a href="MathCards6.png" rel="lightbox[MathCards]"><img border="none" vspace="5" width=200 src="MathCards6.png"></a>
+<a href="MathCards7.png" rel="lightbox[MathCards]"><img border="none" vspace="5" width=200 src="MathCards7.png"></a>
+</td>
+
+<td valign="top">
+<h2>Big Math Flash Cards</h2>
+
+Flip the card just like a real flash card! Simple, clean and easy to use interface. The front side of the card shows the question and the back side of the card shows the possible answers. Just tap to flip the card. Once you answer the question correctly, flick the card off the screen by swiping up.
+<br><br>
+<h3>Features</h3>
+<ul>
+<li>Addition, Subtraction, Multiplication, Division
+<li>Randomly generated numbers
+<li>Score percentage and elapsed time displayed
+<li>Configurable number of questions and difficulty levels
+<li>Other features available via "In App Purchase"
+</ul>
+<br>
+<h3>Paid Features</h3>
+<ul>
+<li>Review Incorrect Answers - review of questions that were answered incorrectly
+<li>View Past Score History - viewing of past scores in a easy to read card format
+<li>Practice Specific Numbers - practicing of a specific number
+<li>Advanced Difficulty Levels - difficulty levels 4, 5 and 6
+<li>Mixed Operation Flash Cards - ability to have cards with mixed operations
+</ul>
+</td></tr>
+
+<tr><td colspan="2">
+<center>
+<h4>Available for the Apple iPhone, iPad and iPod Touch</h4>
+</center>
+</td></tr>
+
+<tr>
+
+<td>
+<center><h4>
+<a target="new" border="none" href="https://itunes.apple.com/us/app/big-math-flash-cards-school/id1156731220?ls=1&mt=8">
+<img border="none" hspace="10" width="60" height="60" align="middle" src="mathcards.png"></a>
+Full Paid School Version<br>
+<a target="new" border="none" href="https://itunes.apple.com/us/app/big-math-flash-cards-school/id1156731220?ls=1&mt=8">
+<img border="none" src="downloadappstore.png"></a>
+</h4></center>
+</td>
+
+<td>
+<center><h4>
+<a target="new" border="none" href="https://itunes.apple.com/us/app/math-cards-flash-flashcards/id929492658?ls=1&mt=8">
+<img border="none" hspace="10" width="60" height="60" align="middle" src="mathcards.png"></a>
+Free version with In App Purchase<br>
+<a target="new" border="none" href="https://itunes.apple.com/us/app/math-cards-flash-flashcards/id929492658?ls=1&mt=8">
+<img border="none" src="downloadappstore.png"></a>
+</h4></center>
+</td>
+
+</tr>
+
+<?php include "footer.html"; ?>
